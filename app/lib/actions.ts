75a03cc853964af853857a88b36c44280cbbef46
onion <kosukekito38@gmail.com>
2025-02-07 09:10:12 +0900

    chapter 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,10 +27,14 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
+    try {
+        await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+    } catch (error) {
+        console.error(error);
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -43,12 +47,16 @@ export async function updateInvoice(id: string, formData: FormData) {
         status: formData.get('status'),
     });
     const amountInCents = amount * 100;
-
-    await sql`
+    try {
+        await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+    } catch (error) {
+        console.error(error);
+    }
+
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
